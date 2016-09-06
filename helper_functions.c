863cd8ad1e6fe0108110f4bf22ddebe82a87a753
Fabio SCHUBERT-GELBCKE <fschuber@e2r3p16.42.fr>
2016-09-06 23:41:44 +0200

    file wiht helper functions

---
+++ helper_functions.c
@@ -0,0 +1,41 @@
+#include "lem_in.h"
+
+void		error()
+{
+	ft_putstr("ERROR");
+	exit(1);
+}
+
+
+int             ft_arrlen(char **arr)
+{
+	int         i;
+	
+	i = 0;
+	while (arr[i])
+		i++;
+	return(i);
+}
+
+int		indexofnode(t_node **graph, char *name)
+{
+	int i;
+
+	i = 0;
+	/*ft_putstr(graph[i]->name);
+	ft_putstr("  ");
+	ft_putstr(name);
+	ft_putstr("\n");*/
+	while (graph[i] && ft_strcmp(graph[i]->name, name))
+	{
+		/*ft_putstr(graph[i]->name);
+		ft_putstr("  ");
+		ft_putstr(name);
+		ft_putstr("\n");*/
+		i++;
+	}
+	if (!graph[i])
+		return (-1);
+	else
+		return (i);
+}
